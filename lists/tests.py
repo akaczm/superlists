b693348169310feb7132acb7fbf19dd8a304bed9
akaczmarek <adam.kaczmarek90@outlook.com>
2016-10-31 08:48:28 +0100

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -25,6 +25,7 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
+
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
@@ -41,7 +42,7 @@ class HomePageTest(TestCase):
         # response is returned as raw bytes thus gets decoded
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_process_post_request(self):
+    def test_home_page_can_save_post_request(self):
         request = HttpRequest()
         request.method = 'POST'
         # use POST to send string to 'item_text'
@@ -51,13 +52,42 @@ class HomePageTest(TestCase):
         # request page with POST with specified values, store in response
         response = home_page(request)
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
         # check if expected value appears in response
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
+        # self.assertIn('A new list item', response.content.decode())
+        # expected_html = render_to_string(
+        #     'home.html',
+        #     {'new_item_text': 'A new list item'}
+        # )
         # check if resulting html is just as we want
-        self.assertEqual(response.content.decode(), expected_html)
+        # self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 # Create your tests here.
