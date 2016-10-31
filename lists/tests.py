9cf036e6e550980ce3faee3d019ffb9addf2b395
akaczmarek <adam.kaczmarek90@outlook.com>
2016-10-31 11:15:25 +0100

    Added URL to handle POST requests. Modified tests accordingly. Removed redundant tests and POST capabilities from homepage.

--- lists/tests.py
+++ lists/tests.py
@@ -42,45 +42,6 @@ class HomePageTest(TestCase):
         # response is returned as raw bytes thus gets decoded
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        # use POST to send string to 'item_text'
-        # POST data is sent as name-value pair
-        request.POST['item_text'] = 'A new list item'
-
-        # request page with POST with specified values, store in response
-        # response = home_page(request)
-        home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_post_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-        # check if expected value appears in response
-        # self.assertIn('A new list item', response.content.decode())
-        # expected_html = render_to_string(
-        #     'home.html',
-        #     {'new_item_text': 'A new list item'}
-        # )
-        # check if resulting html is just as we want
-        # self.assertEqual(response.content.decode(), expected_html)
-
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
-
 
 class ListViewTest(TestCase):
 
@@ -96,4 +57,31 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
-# Create your tests here.
+
+
+class NewListTest(TestCase):
+
+        def test_saving_a_POST_request(self):
+            self.client.post(
+                '/lists/new',
+                data={'item_text': 'A new list item'}
+            )
+            self.assertEqual(Item.objects.count(), 1)
+            new_item = Item.objects.first()
+            self.assertEqual(new_item.text, 'A new list item')
+
+        def test_redirects_after_post(self):
+            response = self.client.post(
+                '/lists/new',
+                data={'item_text': 'A new list item'}
+            )
+            self.assertEqual(response.status_code, 302)
+            self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+            # check if expected value appears in response
+            # self.assertIn('A new list item', response.content.decode())
+            # expected_html = render_to_string(
+            #     'home.html',
+            #     {'new_item_text': 'A new list item'}
+            # )
+            # check if resulting html is just as we want
+            # self.assertEqual(response.content.decode(), expected_html)
